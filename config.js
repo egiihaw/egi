6f93266b2d8e6c84824cacc4064b1ce6937af32a
egiihaw <117686658+egiihaw@users.noreply.github.com>
2022-11-09 19:52:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6285822347348'] // Put your number here
+global.owner = ['6285882949350'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
